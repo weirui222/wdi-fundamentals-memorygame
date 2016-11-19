b423ca29b2eb1f955615f7a012cf1b21e5f1e321
Lyncredible <lyncredible@outlook.com>
2016-11-18 22:24:36 -0800

    JS if-else

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,8 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+if (cardOne === cardTwo) {
+  alert('You found a match!');    
+} else {
+  alert('Sorry, try again.');
+}
